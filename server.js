bcbe7cdd6aa3f61f0da46ed52a0af5c1f05932a4
Alif Mustaqim <42999855+Acigam@users.noreply.github.com>
2023-08-15 01:10:05 +0700

    test origin 3

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const routes = require("./routes/routes");
 const app = express();
 
 const corsOptions = {
-  origin: "https://tkpdplay-fe.onrender.com",
+  origin: process.env.ORIGIN || "http://localhost:3000",
   credentials: true,
   OptionSuccessStatus: 200,
 };
@@ -40,7 +40,7 @@ app.use(express.static("public"));
 const server = require("http").Server(app);
 const io = socketio(server, {
   cors: {
-    origin: "https://tkpdplay-fe.onrender.com",
+    origin: process.env.ORIGIN || "http://localhost:3000",
   },
 });
 
